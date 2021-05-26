847201488ac0b65ccb915c567a9585c23f251920
SheronSuditha <github@sudithakumarasinghe.me>
2021-05-27 03:51:11 +0530

    feat: implemented socket.io server for sensor connectivity

--- socket-server/socket_server.js
+++ socket-server/socket_server.js
@@ -1,21 +1,53 @@
+const e = require('express');
 const express = require('express');
 const fs = require('fs');
 const app = express();
 const http = require('http');
 const PORT = 3005;
-const { Server } = require("socket.io");
+const {
+    Server
+} = require("socket.io");
+
 const io = new Server(server, {
     cors: {
         origin: '*'
     }
 });
 
+
+var sensors = new Array();
+var clients = new Array();
+
 io.on('connection', (socket) => {
     console.log(`New Socket with id: ${socket.id}`)
-    socket.on('hook:init', (data) => {
-        console.log(`Receieved Init: ${socket.id}, ${data.type}`)
+    socket.on('sensor:init', (data) => {
+        sensors.push(socket);
+        logToConsole(`New connection: ${socket.id}#sensor`);
+    })
+
+    socket.on('client:init', (data) => {
+        clients.push(socket);
+        logToConsole(`New connection: ${socket.id}#client`);
+    })
+
+    socket.on('disconnect', () => {
+        if (sensors.find(socket) === undefined)
+            return;
+        else
+            sensors = sensors.filter(sensor => sensor != socket)
+        if (clients.find(socket) === undefined)
+            return;
+        else
+            clients == clients.filter(client => client != socket)
     })
 });
+
+
+function logToConsole(message) {
+    console.log("[SOCKET SERVER] " + message);
+}
+
+
 
 
 app.get('/', (req, res) => {
@@ -25,6 +57,12 @@ app.get('/', (req, res) => {
     })
 });
 
+app.get('/connection-stats', async (req, res) => {
+    res.json({
+        clients,
+        sensors
+    })
+})
 
 server.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
