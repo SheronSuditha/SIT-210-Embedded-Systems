0a39d51e2dc174657c27e38251bbc1fb18e2155d
Sheron Suditha <sheronsuditha1@gmail.com>
2021-05-26 15:40:31 +0530

    refactor: changed schema user syntax

--- backend-api/models/Users.js
+++ backend-api/models/Users.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-module.exports = mongoose.model('user', new mongoose.Schema({
+module.exports = mongoose.model('User', new mongoose.Schema({
     id: String,
     name: String,
     email: String,
