8b008cdb35df0dea8de49a940d07d5ddbc070c6c
Sheron Suditha <github@sudithakumarasinghe.me>
2021-05-29 02:08:44 +0530

    new: added page routes

--- user-frontend/src/App.js
+++ user-frontend/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
-import UserMap from './components/UserMap';
-
+import Homepage from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import DashboardPage from './components/pages/Dashboard';
 
 
 function App() {
 	return (
-		<UserMap />
+		<Router>
+			<div>
+				<Switch>
+					<Route path="/" exact component={Homepage} />
+					<Route path="/dashboard" exact component={DashboardPage} />
+				</Switch>
+			</div>
+		</Router>
+
 	)
 }
 
