1aa2ad0569fce7d521bf5968fe7beee173008505
Sheron Suditha <github@sudithakumarasinghe.me>
2021-06-01 12:55:16 +0530

    new(redux): sensors redux store - actions

---
+++ user-frontend/src/redux/actions/sensors/index.js
@@ -0,0 +1,9 @@
+export const addSensorsToRedux = (payload) => ({
+    type: "ADD_SENSORS_REDUX",
+    payload
+})
+
+export const updateSensorInRedux = (payload) => ({
+    type: "UPDATE_SENSOR_REDUX",
+    payload
+})
