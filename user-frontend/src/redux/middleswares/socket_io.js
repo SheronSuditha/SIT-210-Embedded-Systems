dc206a520557a35230ea11492ad93667bdd66a5d
Sheron Suditha <github@sudithakumarasinghe.me>
2021-06-01 17:09:09 +0530

    new(redux): custom socketio middleware

--- user-frontend/src/redux/middleswares/socket_io.js
+++ user-frontend/src/redux/middleswares/socket_io.js
@@ -14,27 +14,32 @@ export async function connect_socket() {
 
 
 /* MIDDLEWARE SECTION */
-export const socketMiddleWare = store => next => action => {
-    switch (action) {
+export const socketMiddleWare = (store) => (next) => (action) => {
+    console.log(action.type)
+    switch (action.type) {
         case 'START_RT_SOCKET':
+            console.log("RUNNING START RT")
             connect_socket();
             break;
     }
 
     next(action);
 
+    if (socket === null) return;
     //ack event from relay server
-    socket.on('client:ack', () => {
-        store.dispatch({
-            type: "INIT_RT_SOCKET",
-            payload: {
-                isEnabled: true,
-                socketId: socket.id
-            }
-        })
-    })
+    // socket.on('client:ack', () => {
+    //     console.log("SOCKET ACK")
+    //     store.dispatch({
+    //         type: "INIT_RT_SOCKET",
+    //         payload: {
+    //             isEnabled: true,
+    //             socketId: socket.id
+    //         }
+    //     })
+    // })
 
     socket.on('client:data:relay', (data) => {
+        console.log("RELAY")
         const { id, ultrasonic_state, photosensor_state } = data;
         store.dispatch({
             type: "UPDATE_RT_DATA_RELAY",
