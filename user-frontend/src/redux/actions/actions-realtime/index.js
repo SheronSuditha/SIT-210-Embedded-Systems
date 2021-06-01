863bff9710cb89949407988a67425c712765a8a0
Sheron Suditha <github@sudithakumarasinghe.me>
2021-06-01 17:09:52 +0530

    new(redux): realtime action

--- user-frontend/src/redux/actions/actions-realtime/index.js
+++ user-frontend/src/redux/actions/actions-realtime/index.js
@@ -1,5 +1,5 @@
 export const enableRealtimeSocket = (payload) => ({ type: "INIT_RT_SOCKET", payload }) //start socket conenction
 
-export const startRealtimeScoket = (payload) => ({ type: "START_RT_SOCKET", payload }) // set socket connection
+export const startRealtimeScoket = (payload) => ({ type: "START_RT_SOCKET", payload }) // MIDDLEWARE set socket connection
 
 export const socketDataRelay = (payload) => ({ type: "UPDATE_RT_DATA_RELAY", payload }) // update dash on data reay
